8ba358f8c56aba3994dd321f510ee9c159949bde
rayan511x55x <rayan511x55x@example.com>
2025-07-22 16:46:41 -0400

    Remove internal folder and refactor imports

---
+++ minecraft/protocol/info.go
@@ -0,0 +1,9 @@
+package protocol
+
+const (
+	// CurrentProtocol is the current protocol version for the version below.
+	CurrentProtocol = 827
+	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
+	CurrentVersion = "1.21.100.24"
+)
+
